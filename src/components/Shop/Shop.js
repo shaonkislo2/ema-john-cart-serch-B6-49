54132ca72ae705587db76865e638325a1910b913
shaonkislo <shaon6340@agmail.com>
2023-05-03 12:45:48 +0600

    pagiantion complete

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,13 +9,6 @@ import Product from "../Product/Product";
 import "./Shop.css";
 import { Link, useLoaderData } from "react-router-dom";
 
-/* 
-count : loaded
-perPage (size): 10
-pages: count / perPage
-currentPage (page)
-
-*/
 
 const Shop = () => {
   // const {products, count} = useLoaderData();
@@ -46,15 +39,30 @@ const Shop = () => {
   useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
-    for (const id in storedCart) {
-      const addedProduct = products.find((product) => product._id === id);
-      if (addedProduct) {
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        savedCart.push(addedProduct);
+    const ids = Object.keys(storedCart);
+    console.log(ids);
+    fetch('http://localhost:5000/productsByIds', {
+      method: 'POST',
+      headers:{
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(ids)
+    })
+    .then(res => res.json())
+    .then(data => {
+      for (const id in storedCart) {
+        const addedProduct = data.find((product) => product._id === id);
+        if (addedProduct) {
+          const quantity = storedCart[id];
+          addedProduct.quantity = quantity;
+          savedCart.push(addedProduct);
+        }
       }
-    }
-    setCart(savedCart);
+      setCart(savedCart); 
+    })
+    
+    
+   
   }, [products]);
 
   const handleAddToCart = (selectedProduct) => {
@@ -99,7 +107,7 @@ const Shop = () => {
           {
             [...Array(pages).keys()].map(number => <button
             key={number}
-            className={page === number && 'selected'}
+            className={page === number ?'selected' : ''}
             onClick={() => setPage(number)}
             >
               {number }
